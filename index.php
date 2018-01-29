44fd5039317255db43831b1feff38e8b3bbb1ba8
manyang901 <gzmanyang@gmail.com>
2018-01-29 10:19:01 +0800

    refactor(index):small progress of refactor

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 这是由Viosey 基于 Google Material Design 开发的 Typecho 主题
+ * 由Manyang901继续维护和更新
+ *
+ * @package Theme.Material
+ * @author Viosey&manyang901
+ * @version 1.3.0
+ * @link https://github.com/manyang901/material
+ */
+
+$this->need('header.php'); ?>
+	
+	<div>
+		
+		<main>
+			
+			<header>
+				
+			</header>
+			
