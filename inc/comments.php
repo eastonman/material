80a837b77d5edd4c60a96ed0405e8c7db2890f9a
manyang901 <gzmanyang@gmail.com>
2018-02-10 20:10:27 +0800

    feat(comment):finish optimal comment list & beautify comment list

--- inc/comments.php
+++ inc/comments.php
@@ -4,70 +4,92 @@
     $commentClass = '';
     $commentLevelClass = $comments->_levels > 0 ? ' comment-child' : ' comment-parent';  //评论层数大于0为子级，否则是父级
 ?>
-    <div id="li-<?php $comments->theId(); ?>" class="comment  mdui-card comment-body<?php
-    if ($comments->_levels > 0) {
-        echo ' comment-child';
-        $comments->levelsAlt(' comment-level-odd', ' comment-level-even');
-    } else {
-        echo ' comment-parent';
-    }
-    $comments->alt(' comment-odd', ' comment-even');
-    echo $commentClass; ?>">
+	<!--Mdui 框架 mdui-card overflow是 hidden 此处强制改为 visible-->
+    <div 
+    	id="li-<?php $comments->theId(); ?>"
+    	style="overflow: visible;"
+    	class="comment  mdui-card comment-body 
+    		<?php
+    			if ($comments->_levels > 0) {
+        			echo ' comment-child';
+        			$comments->levelsAlt(' comment-level-odd', ' comment-level-even');
+    			} else {
+        			echo ' comment-parent';
+    			}
+    			$comments->alt(' comment-odd', ' comment-even');
+    			echo $commentClass; 
+    		?>
+    	"
+    >
 
     <!-- Comment info -->
     <div class="comment header mdui-card-header">
 
         <!-- Comment avatar -->
-        
-            <?php $comments->gravatar(52,'X',NULL,"mdui-card-header-avatar"); ?>
-        
-
+        <div class="mdui-card-header-avatar mdui-icon mdui-btn-icon">
+            <?php $comments->gravatar(42); ?>
+        </div>
+    
         <!-- Comment author -->
-        <div class="comment__author">
+        
             <!--Commenter name -->
             <div class="mdui-card-header-title"><?php $comments->author(); ?>&nbsp;</div>
             <!--Comment date -->
             <div class="mdui-card-header-subtitle"><?php $comments->date('Y-m-d, H:i'); ?></div>
-        </div>
     </div>
 
     <!-- Comment content -->
-    <div class="comment__text">
+    <div class="mdui-card-content">
         <?php $comments->content(); ?>
     </div>
 
     <!-- Comment actions -->
-    <nav class="comment__actions">
+    <nav class="mdui-card-actions">
+
         <!-- like -->
-        <button id="comment-like-button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                <i class="material-icons" role="presentation">thumb_up</i>
-                <span class="visuallyhidden">like comment</span>
-            </button>
+        <button id="comment-like-button" class="mdui-btn mdui-ripple mdui-btn-icon" >
+                <i class="mdui-icon material-icons mdui-color-white" role="presentation">thumb_up</i>
+                <span class="mdui-hidden">like comment</span>
+        </button>
+
         <!-- dislike -->
-        <button id="comment-dislike-button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                <i class="material-icons" role="presentation">thumb_down</i>
-                <span class="visuallyhidden">dislike comment</span>
-            </button>
+        <button id="comment-dislike-button" class="mdui-btn mdui-ripple mdui-btn-icon">
+                <i class="mdui-icon material-icons mdui-color-white" role="presentation">thumb_down</i>
+                <span class="mdui-hidden">dislike comment</span>
+        </button>
+
         <!-- reply -->
-<?php $comments->reply('<button id="comment-reply-button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-<i class="material-icons" role="presentation">forum</i>
-<span class="visuallyhidden">reply comment</span>
-</button>'); ?>
+		<?php $comments->reply('
+			<button id="comment-reply-button" class="mdui-btn mdui-ripple mdui-btn-icon">
+				<i class="mdui-icon material-icons mdui-color-white" role="presentation">forum</i>
+				<span class="mdui-hidden">reply comment</span>
+			</button>
+		'); ?>
+
         <!-- share -->
-        <button id="comment-share-<?php $comments->theId(); ?>-button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                <i class="material-icons" role="presentation">share</i>
-                <span class="visuallyhidden">share comment</span>
-            </button>
-        <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect" for="comment-share-<?php $comments->theId(); ?>-button">
-            <a class="md-menu-list-a" target="view_window" href="<?php $comments->permalink(); ?>">
-                <li class="mdl-menu__item">Open in New Tab</li>
-            </a>
-            <a class="md-menu-list-a" href="https://twitter.com/intent/tweet?text=<?php $comments->content(); ?>+from&url=<?php $comments->permalink(); ?>">
-                <li class="mdl-menu__item">Share to Twitter</li>
-            </a>
-            <a class="md-menu-list-a" href="https://plus.google.com/share?url=<?php $comments->permalink(); ?>" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
-                <li class="mdl-menu__item">Share to Google+</li>
-            </a>
+        <button id="comment-share-<?php $comments->theId(); ?>-button" class="mdui-btn mdui-ripple mdui-float-right mdui-btn-icon" mdui-menu="{target: '#comment-share-list-<?php $comments->theId(); ?>'}">
+                <i class="mdui-icon material-icons mdui-color-white" role="presentation">share</i>
+                <span class="mdui-hidden">share comment</span>
+        </button>
+
+        <ul id="comment-share-list-<?php $comments->theId(); ?>" class="mdui-menu" for="comment-share-<?php $comments->theId(); ?>-button">
+        	<li class="mdui-menu-item">
+            	<a class="md-menu-list-a" target="view_window" href="<?php $comments->permalink(); ?>">
+                Open in New Tab
+            	</a>
+        	</li>
+
+        	<li class="mdui-menu-item">
+            	<a class="md-menu-list-a" href="https://twitter.com/intent/tweet?text=<?php $comments->content(); ?>+from&url=<?php $comments->permalink(); ?>">
+                Share to Twitter
+            	</a>
+            </li>
+
+            <li class="mdui-menu-item">
+            	<a class="md-menu-list-a" href="https://plus.google.com/share?url=<?php $comments->permalink(); ?>" onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
+                Share to Google+
+            	</a>
+            </li>
         </ul>
     </nav>
 
@@ -106,77 +128,83 @@
             <!-- If user has login -->
             <?php if ($this->user->hasLogin()): ?>
 
-            <!-- Display user name & logout -->
-            <p style="color:#8A8A8A;" class="mdui-typo">Logged in as
-                <a href="<?php $this->options->adminUrl(); ?>" style="font-weight:400"><?php $this->user->screenName(); ?></a>.
-                <a href="<?php $this->options->logoutUrl(); ?>" title="Logout" style="font-weight:400">Logout &raquo;</a>
-            </p>
+            	<!-- Display user name & logout -->
+            	<p style="color:#8A8A8A;" class="mdui-typo">Logged in as
+                	<a href="<?php $this->options->adminUrl(); ?>" style="font-weight:400"><?php $this->user->screenName(); ?></a>.
+                	<a href="<?php $this->options->logoutUrl(); ?>" title="Logout" style="font-weight:400">Logout &raquo;</a>
+            	</p>
 
             <!-- If user didn't login -->
             <?php else: ?>
 
-            <!-- Input name -->
-            <div class="login-form-group">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input type="text" name="author" class="mdl-textfield__input login-input-info" />
-                    <label for="author" class="mdl-textfield__label">
+            	<!-- Input name -->
+            	<div class="login-form-group">
+                	<div class="mdui-textfield mdui-textfield-floating-label">
+                    	<label for="author" class="mdui-textfield-label">
                                     <?php if ($this->options->langis == '0'): ?>
                                         Name*
                                     <?php elseif ($this->options->langis == '1'): ?>
                                         昵称*
                                     <?php endif; ?>
-                                </label>
-                </div>
-            </div>
+                        </label>
+                        <input type="text" name="author" class="mdui-textfield-input" />
+                    	
+                	</div>
+            	</div>
 
-            <!-- Input email -->
-            <div class="login-form-group">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input type="email" name="mail" class="mdl-textfield__input login-input-info" />
-                    <label for="mail" class="mdl-textfield__label">
+            	<!-- Input email -->
+            	<div class="login-form-group">
+                	<div class="mdui-textfield mdui-textfield-floating-label">
+                    	
+                    	<label for="mail" class="mdui-textfield-label">
                                     <?php if ($this->options->langis == '0'): ?>
                                         Email*
                                     <?php elseif ($this->options->langis == '1'): ?>
                                         邮箱*
                                     <?php endif; ?>
-                                </label>
-                </div>
-            </div>
+                        </label>
+                        <input type="email" name="mail" class="mdui-textfield-input" />
+                	</div>
+            	</div>
 
-            <!-- Input website -->
-            <div class="login-form-group">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input type="url" name="url" id="visitor-url" class="mdl-textfield__input login-input-info" />
-                    <!--  placeholder="http://"-->
-                    <label for="url" class="mdl-textfield__label">
+            	<!-- Input website -->
+            	<div class="login-form-group">
+                	<div class="mdui-textfield mdui-textfield-floating-label">
+                    	
+                    	<!--  placeholder="http://"-->
+                    	<label for="url" class="mdui-textfield-label">
                                     <?php if ($this->options->langis == '0'): ?>
                                         Website
                                     <?php elseif ($this->options->langis == '1'): ?>
                                         网站
                                     <?php endif; ?>
-                                </label>
-                </div>
-            </div>
+                        </label>
+                        <input type="url" name="url" id="visitor-url" class="mdui-textfield-input " />
+                	</div>
+            	</div>
+
             <?php endif; ?>
 
+
             <!-- Input comment content -->
-            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" id="comment-input-div">
-                <textarea name="text" rows="1" id="comment" class="mdl-textfield__input"></textarea>
-                <label for="comment" class="mdl-textfield__label">
+            <div class="mdui-textfield mdui-textfield-floating-label" id="comment-input-div">
+                
+                <label for="comment" class="mdui-textfield-label">
                             <?php if ($this->options->langis == '0'): ?>
                                 Join the discussion
                             <?php elseif ($this->options->langis == '1'): ?>
                                 加入讨论吧...
                             <?php endif; ?>
-                        </label>
+                </label>
+                <textarea name="text"  id="comment" class="mdui-textfield-input"></textarea>
             </div>
 
             <!-- Submit comment content button -->
-<?php $comments->reply('
-                    <button id="comment-button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
+			<?php $comments->reply('
+                    <button id="comment-button" class="mdui-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
                         <i class="material-icons mdui-icon" role="presentation">check</i><span class="visuallyhidden">add comment</span>
                     </button>'); ?>
-            <div class="mdl-tooltip" for="comment-button">Submit</div>
+            <div class="mdui-tooltip" for="comment-button">Submit</div>
 
         </form>
     </div>
