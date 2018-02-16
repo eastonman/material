c91951d5eba7d5adad3fd4737f1940e1236d2cf1
manyang901 <gzmanyang@gmail.com>
2018-02-16 20:25:48 +0800

    fix(sidebar): fix header height & post thumbnail height

--- inc/footer.php
+++ inc/footer.php
@@ -3,20 +3,20 @@
 <div class="mdui-container-fluid">
 		<!--Footer-->
         <footer class="mdui-row mdui-shadow-2 mdui-color-white" id="bottom">
-            
-            <!--Footer Left SNS Icons Begin-->            
+
+            <!--Footer Left SNS Icons Begin-->
             <div class="mdui-col-xs-12 mdui-col-md-4 mdui-text-center footer-sns">
         		<!--Twitter Icon-->
                 <?php if (!empty($this->options->footersns) && in_array('ShowTwitter', $this->options->footersns)) : ?>
                     <a href="<?php $this->options->TwitterURL() ?>" class="mdui-color-white" target="view_window">
-                    	<i class="mdui-icon icon-twitter_10 "></i>                    	
+                    	<i class="mdui-icon icon-twitter_10 "></i>
                     </a>
                 <?php else: ?>
                 <?php endif; ?>
-                
-                    	
-                <!-- FaceBook Icon --> 
-                <?php if (!empty($this->options->footersns) && in_array('ShowFacebook', $this->options->footersns)) : ?>             
+
+
+                <!-- FaceBook Icon -->
+                <?php if (!empty($this->options->footersns) && in_array('ShowFacebook', $this->options->footersns)) : ?>
                 	<a href="<?php $this->options->FacebookURL() ?>" class="mdui-color-white" target="view_window">
                 		<i class="mdui-icon icon-facebook_6" > </i>
                 	</a>
@@ -24,7 +24,7 @@
                 <?php endif; ?>
 
                 <!-- Github Icon -->
-                <?php if (!empty($this->options->footersns) && in_array('ShowGithub', $this->options->footersns)) : ?>               
+                <?php if (!empty($this->options->footersns) && in_array('ShowGithub', $this->options->footersns)) : ?>
                     <a href="<?php $this->options->GithubURL() ?>" class="mdui-color-white" target="view_window">
                     	<i class="mdui-icon icon-github" ></i>
                     </a>
@@ -33,7 +33,7 @@
 
 
                 <!--Telegram Icon-->
-                <?php if (!empty($this->options->footersns) && in_array('ShowTelegram', $this->options->footersns)) : ?>                  
+                <?php if (!empty($this->options->footersns) && in_array('ShowTelegram', $this->options->footersns)) : ?>
                     <a href="<?php $this->options->TelegramURL() ?>" class="mdui-color-white" target="view_window">
                         <i class="mdui-icon icon-telegram" ></i>
                     </a>
@@ -52,14 +52,14 @@
 						<i class="mdui-icon icon-youtube" ></i>
                     </a>
                 <?php endif;?>
-                	
+
                 <!-- Steam Icon -->
                 <?php if (!empty($this->options->footersns) && in_array('ShowSteam', $this->options->footersns)) : ?>
 					<a href="<?php $this->options->SteamURL() ?>" class="mdui-color-white" target="view_window">
 						<i class="mdui-icon icon-steam" ></i>
                     </a>
                 <?php endif;?>
-                	
+
                 <!-- Niconico Icon -->
                 <?php if (!empty($this->options->footersns) && in_array('ShowNiconico', $this->options->footersns)) : ?>
 					<a href="<?php $this->options->NiconicoURL() ?>" class="mdui-color-white" target="view_window">
@@ -68,7 +68,7 @@
                 <?php endif;?>
 
             </div>
-            <!--Footer Left SNS END--> 
+            <!--Footer Left SNS END-->
 
             <!--copyright-->
             <div class="mdui-col-xs-12 mdui-col-md-4 mdui-text-center" id="copyright">
@@ -87,7 +87,7 @@
                 </div>
             </div>
         </footer>
-	
+
 </div>
 
 
@@ -98,7 +98,7 @@
 </main>
 
 <script src="//cdn.bootcss.com/mdui/0.4.0/js/mdui.min.js"></script>
-<script async src="https://cdn.bootcss.com/vanilla-lazyload/10.4.2/lazyload.min.js"></script>
+<script src="https://cdn.bootcss.com/vanilla-lazyload/10.4.2/lazyload.min.js"></script>
 <script src="<?php $this->options->themeUrl('js/sidebar.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('js/lazyload.js'); ?>"></script>
 
